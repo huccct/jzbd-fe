7d91318a3dec033752810bf618b93adc424b9c50
huccct <2972223145@qq.com>
2023-04-11 11:09:35 +0800

    feat:初始化项目

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
     <nav>
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>
+      |
       <router-link to="/about">About</router-link>
     </nav>
     <router-view />
