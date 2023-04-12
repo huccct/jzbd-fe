6fa9f255b86485a0d17e8af4881f3d5d8025313b
huccct <2972223145@qq.com>
2023-04-12 14:30:49 +0800

    feat(components): 更新footer组件

--- src/components/bottom/bottom.vue
+++ src/components/bottom/bottom.vue
@@ -1,9 +1,30 @@
 <template>
   <div class="foot-menu">
-    <el-footer> </el-footer>
+    <el-footer>
+      <div class="footer-content-top"></div>
+      <div class="divider">
+        <el-divider></el-divider>
+      </div>
+      <div class="footer-content-bottom">
+        <span>{{ companyInfo }}</span>
+      </div>
+    </el-footer>
   </div>
 </template>
-<style scoped>
+
+<script>
+export default {
+  name: 'bottom',
+  data() {
+    return {
+      companyInfo:
+        'copyright ◎2014- 2022 scodpt.com青岛上合企业创新产业园All Rights Reserved京ICP备2022015806号'
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
 .foot-menu {
   position: absolute;
   bottom: 0;
@@ -11,8 +32,23 @@
   color: #fff;
   width: 100%;
   background-color: rgba(136, 136, 136, 0.48);
-  height: 90px;
+  height: 190px;
   line-height: 90px;
   text-align: center;
+}
+.divider {
+  width: 100%;
+  position: absolute;
+  left: 0;
+  bottom: 0;
+}
+.footer-content-bottom span {
+  color: #fff;
+  font-size: 14px;
+  position: relative;
+  bottom: -135px;
+  left: 0;
+  right: 0;
+  margin: 0 auto;
 }
 </style>
