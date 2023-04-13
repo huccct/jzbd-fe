efbc7564caa44172e86767a787c1ce175deb7ae6
Mr.S <1275796924@qq.com>
2023-04-13 08:53:27 +0800

    fix(components): 修复园区服务显示bug

---
+++ src/components/ourservice/pressannouncement.vue
@@ -0,0 +1,25 @@
+<!--
+ * @Author: hayyot
+ * @Date: 2023-04-13 08:45:56
+ * @Description: 铁沸物
+ * @FilePath: \jzbd-fe\src\components\ourservice\pressannouncement.vue
+-->
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'JzbdFePressannouncement',
+
+  data() {
+    return {};
+  },
+
+  mounted() {},
+
+  methods: {}
+};
+</script>
+
+<style lang="scss" scoped></style>
