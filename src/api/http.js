62e12ff2d4dd259e6e2d54185e563d1585640ce6
huccct <2972223145@qq.com>
2023-04-18 15:18:38 +0800

    fix(*): 修复轮播图部分抖动问题

--- src/api/http.js
+++ src/api/http.js
@@ -10,7 +10,7 @@ import store from '../store/index';
 
 // 环境的切换
 if (process.env.NODE_ENV == 'development') {
-  axios.defaults.baseURL = 'http://127.0.0.1:4523/m1/2574207-0-default';
+  axios.defaults.baseURL = 'http://114.116.18.241:8080';
 } else if (process.env.NODE_ENV == 'debug') {
   axios.defaults.baseURL = '';
 } else if (process.env.NODE_ENV == 'production') {
